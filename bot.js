334cb4e18fda8f7c1f54c2f5b164e5f369be95b7
jackyboy720720 <42346338+jackyboy720720@users.noreply.github.com>
2018-08-13 16:35:35 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,8 +6,8 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
+    if (message.content === 'Paul') {
+    	message.reply('Wassup motherfuckers!');
   	}
 });
 
